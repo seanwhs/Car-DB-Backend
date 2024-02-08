dd0f226f25ebbe99737846dcd28f66c8121cb1b1
seanwhs <seanwhs@gmail.com>
2024-02-08 17:40:43 +0800

    Implemented CORS opening up localhost:3000

--- Chapter5 Securing Backend/backend/src/main/java/carDB/backend/security/SecurityConfig.java
+++ Chapter5 Securing Backend/backend/src/main/java/carDB/backend/security/SecurityConfig.java
@@ -1,6 +1,6 @@
 //SecurityConfig.java
 package carDB.backend.security;
-
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,6 +14,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -24,13 +27,12 @@ public class SecurityConfig {
     private final AuthEntryPoint exceptionHandler;
 
     public SecurityConfig(
-        UserDetailsServiceImpl userDetailsService,
-        AuthenticationFilter authenticationFilter,
-        AuthEntryPoint exceptionHandler
-        ) {
+            UserDetailsServiceImpl userDetailsService,
+            AuthenticationFilter authenticationFilter,
+            AuthEntryPoint exceptionHandler) {
         this.userDetailsService = userDetailsService;
         this.authenticationFilter = authenticationFilter;
-        this.exceptionHandler=exceptionHandler;
+        this.exceptionHandler = exceptionHandler;
     }
 
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -42,17 +44,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf((csrf) -> csrf.disable());
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
         http.sessionManagement((sessionManagement) -> sessionManagement
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http
-            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-            .requestMatchers(HttpMethod.POST,"/login").permitAll()
-            .anyRequest().authenticated())
-            .addFilterBefore(
-                authenticationFilter, UsernamePasswordAuthenticationFilter.class
-                )
-            .exceptionHandling((exceptionHandling) -> exceptionHandling.
-                authenticationEntryPoint(exceptionHandler));
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(
+                        authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling((exceptionHandling) -> exceptionHandling.authenticationEntryPoint(exceptionHandler));
         return http.build();
     }
 
@@ -65,5 +66,20 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
+    }
+
+    // Add Global CORS filter inside the class
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        // localhost:3000 for REACT is allowed
+        config.setAllowedOrigins(Arrays.asList ("http://localhost:3000"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(false);
+        config.applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
